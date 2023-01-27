6f57bbc28b3d790095e67d34e14ddd65722ff49f
Andreiu Dan <dan.andreiu@ulbsibiu.ro>
2023-01-27 22:53:55 +0200

    added product entity + ProductBean and some methods

---
+++ src/main/java/com/pos/proiectpos/entities/Product.java
@@ -0,0 +1,51 @@
+package com.pos.proiectpos.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    private Long id;
+
+    public int quantity;
+
+    public String name;
+
+    public float price;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
